f5e99d1a218768b09887de369918ef7794bc3a57
이은재 <>
2024-02-16 09:52:40 +0900

    first commit

---
+++ src/main/java/org/zerock/githubpractice/GitHubPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.githubpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/githubpractice/GitHubPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.githubpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
